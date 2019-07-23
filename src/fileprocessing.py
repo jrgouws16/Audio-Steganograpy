c2ece207900e1a7a6c2020c12ce110694996b5df
Johan Gouws <u16033915@tuks.co.za>
2019-07-23 13:39:14 +0200

    Made fileprocessing same as LSB fileprocessing

--- src/fileprocessing.py
+++ src/fileprocessing.py
@@ -2,39 +2,86 @@ import tkinter as tk
 from tkinter import filedialog
 import sys
 import struct
+import SignalsAndSlots
+
+# Create a signal to emit the progress of extracting samples
+signalExtract = SignalsAndSlots.SigSlot()
+
+# Create a signal to emit the progress of extracting samples
+signalReadMsg = SignalsAndSlots.SigSlot()
+
+# Create a signal to emit the progress of writing to mesage file
+signalWriteMsg = SignalsAndSlots.SigSlot()
 
 ########################################################################################################################
 #####################                          General usefull functions                    ############################
 ########################################################################################################################
-def browseFilename():
-    root = tk.Tk()  # Create a tkinter main window
-    root.withdraw()  # Hide the main window
-    return filedialog.askopenfilename()  # Allow user to select the file path through file browsing
+def openFile():
+    # Create a tkinter main window
+    root = tk.Tk()
+
+    # Hide the main window                           
+    root.withdraw()  
+
+    # Allow user to select the file path through file browsing                            
+    return filedialog.askopenfilename()          
+
+def saveFile():
+    root = tk.Tk()
+    root.withdraw()
+    return filedialog.asksaveasfilename(initialdir = "/",
+                                        title = "Select file",
+                                        filetypes = (("(.wav) files","*.wav"),
+                                                     ("all files","*.*")))
+
+def saveMessage():
+    root = tk.Tk()
+    root.withdraw()
+    return filedialog.asksaveasfilename(initialdir = "/",
+                                        title = "Select file",
+                                        filetypes = (("(.txt) files","*.txt"), 
+                                                     (".pdf) files","*.pdf"),
+                                                     ("all files","*.*")))
 
 ########################################################################################################################
 #####################               Functions for reading and writing to message file       ############################
 ########################################################################################################################
 
 # Takes any message file and returns a integer list of the bits
-def getMessageBits():
-    root = tk.Tk()                                 # Create a tkinter main window
-    root.withdraw()                                # Hide the main window
-    file_path = filedialog.askopenfilename()       # Allow user to select the file path through file browsing
-    file = open(file_path, 'rb')                   # Open the file to be read
+def getMessageBits(file_path):
+    # Open the file to be read and get the length
+    file = open(file_path, 'rb')                   
+    messageLength = len(file.read())
+    
+    # Reopen file to read from beginning
+    file = open(file_path, 'rb') 
 
-    totalSamples = ''                              # Samples to be returned
+    # Samples to be returned                  
+    totalSamples = '' 
+    
+    totalBytes = 0
+
     while (1):
-        byte = file.read(1)                        # Read one byte of the file
+        # Read one byte of the file
+        byte = file.read(1)                        
+        totalBytes += 1
+        
+        # Transmit signal to display progress on GUI
+        signalReadMsg.trigger.emit((totalBytes+1)*100/messageLength)
 
-        if (len(byte) == 0):                       # Break if the end of the file is reached
+        # Break if the end of the file is reached
+        if (len(byte) == 0):                       
             break
 
-        sample = "{0:08b}".format(int.from_bytes(byte, byteorder=sys.byteorder))    # Format in 8-bit samples
-        totalSamples += sample                                                      # Add to total samples
+        # Format in 8-bit string samples
+        sample = "{0:08b}".format(int.from_bytes(byte, byteorder=sys.byteorder))
+        
+        totalSamples += sample                                                     
 
     file.close()
 
-    return list(map(int, totalSamples))                                             # Return integer samples
+    # Return integer list of samples
+    return list(map(int, totalSamples))                                             
 
 
 # Convert an eight bit string to list of eight integers
@@ -42,14 +89,22 @@ def messageToBinary(message):
     return list(map(int, ''.join('{0:08b}'.format(ord(x), 'b') for x in message)))
 
 # Write the binary message to a file
-def writeMessageBitsToFile(totalMessageBits):
-    file_path = browseFilename()
-    file = open(file_path, 'wb')                        # Open the file to be written
+def writeMessageBitsToFile(totalMessageBits, file_path):
+    file = open(file_path, 'wb')                       
+
+    # Number of messsage bits to keep track of progress on GUI
+    originalSize = len(totalMessageBits)
 
     # Write eight bits at a time and delete the eight bits
     while (len(totalMessageBits) > 0):
-        file.write((int(samples[0:8], 2).to_bytes(1, byteorder = sys.byteorder)))
-        samples = samples[8:]
+        # Convert eight bit sample to integer value and convert to a byte format
+        file.write((int(totalMessageBits[0:8], 2).to_bytes(1, byteorder = sys.byteorder)))
+        
+        # Delete the byte that was written
+        totalMessageBits = totalMessageBits[8:]
+        
+        # Send signal to update GUI
+        signalWriteMsg.trigger.emit(100-100.0*len(totalMessageBits)/originalSize)
 
     file.close()
 
@@ -64,6 +119,8 @@ def writeMessageBitsToFile(totalMessageBits):
 def extractWaveSamples(waveObject):
     samplesChannelOne = []
     samplesChannelTwo = []
+    
+    # Get the wave file parameters
     nchannels, sampwidth, framerate, nframes, comptype, compname = waveObject.getparams()
 
     # Read two bytes for mono (<1h) in little endian format
@@ -71,24 +128,32 @@ def extractWaveSamples(waveObject):
     format = '<{}h'.format(nchannels)
 
     for index in range(nframes):
-        frame = self.audio.readframes(1)            # Read frame
-        data = struct.unpack(format, frame)         # Unpack frame from sixteen bit sample to an integer
-        samplesChannelOne.append(data[0])           # First (left) channel only
+        # Read one frame
+        frame = waveObject.readframes(1)
 
-        if (samplesChannelOne[-1] == -32768):       # Prevents overflow when inserting the message
+        # Unpack the frame to integer value            
+        data = struct.unpack(format, frame) 
+        
+        # First channel value added to channel one
+        samplesChannelOne.append(data[0])         
+
+        # Prevents overflow when inserting the message
+        if (samplesChannelOne[-1] == -32768):       
             samplesChannelOne[-1] += 1
 
+        # Do the second channel
         if (nchannels == 2):
             samplesChannelTwo.append(data[1])
 
             if (samplesChannelTwo[-1] == -32768):
                 samplesChannelTwo[-1] += 1
 
-        return samplesChannelOne, samplesChannelTwo
+        signalExtract.trigger.emit((index+1) * 100 / nframes)
+
+    return samplesChannelOne, samplesChannelTwo
 
 # Prints the wave file parameters in the terminal
 def returnParameters(self):
-    # Get input file parameters
     print("Channels:", self.nchannels)
     print("Sample width (bytes per sample):", self.sampwidth)
     print("Framerate (sampling rate):", self.framerate)
