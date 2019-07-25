897c5147f96169a5b372da50f4ae2cc39257e030
Johan Gouws <u16033915@tuks.co.za>
2019-07-25 18:13:41 +0200

    Debugging GA

--- src/GA_driver.py
+++ src/GA_driver.py
@@ -5,10 +5,8 @@ Created on Tue Jul 23 14:10:15 2019
 @author: project
 """
 
-import fileprocessing as fp
 import geneticAlgorithm as GA
-import wave
-
+'''
 print("##################################################")
 print("######      Tesitng generate population     ######")
 print("##################################################")
@@ -25,15 +23,7 @@ print("Population is:")
 for i in population:
     print(i)
     
-print("##################################################")
-print("######    Tesitng crossOverOnes function    ######")
-print("##################################################")
-      
-print("Original Sample:", "{0:016b}".format(samples[0]))      
-print("Crossover start = -15, end = -1", "".join(GA.crossOverOnes(-15,-1, list("{0:016b}".format(samples[0])))))
-
-print("Original Sample:", "{0:016b}".format(samples[0]))      
-print("Crossover start = 1, end = 15", "".join(GA.crossOverOnes(-4,-3, list("{0:016b}".format(samples[0])))))
+'''
 
 print("##################################################")
 print("######    Tesitng generate next generation  ######")
@@ -43,17 +33,21 @@ sample = '1000000110000010'
       
 print("The original sample is: ", sample)
 
-population = GA.generatePopulation(sample, '1')
+population, replacedBits = GA.generatePopulation(sample, '1')
 
+print("Replaced bits are:", replacedBits)
 print("Population after one is inserted:")
+
+population.reverse()
+replacedBits.reverse()
 
 for i in population:
     print(i)
- 
-print(population)       
-population = GA.generateNextGeneration(population)
+      
+population = GA.generateNextGeneration(population, replacedBits, 1)
 
 print("Next generation:")
+population.reverse()
 
 for i in population:
     print(i)
--- src/geneticAlgorithm.py
+++ src/geneticAlgorithm.py
@@ -7,25 +7,20 @@ Created on Tue Jul 23 14:10:15 2019
 
 # Function that takes a 16-bit audio sample and generates 5 chromosomes
 # of the sample, by inserting the bit in LSB positions 4 - 8
+# The function also returns the data bits that were replaced, as it will be 
+# used in later GA operations
 def generatePopulation(sample, bit):
     
     population = []
+    replacedBits = []
     
     for i in range(8, 13):
         addSample = list(sample)
+        replacedBits.append(addSample[i])
         addSample[i] = bit
         population.append("".join(addSample))
         
-    return population
-
-# Performs crossover operation for the sample with 16-bit samples of one
-# Ones is inserted in position start to position stop in the sample   
-def crossOverOnes(start, stop, sample):
-    
-    for i in range(start, stop):
-        sample[i] = '1'
-   
-    return sample
+    return population, replacedBits
 
 # Performs crossover operation for the sample with 16-bit samples of zero
 # Zeros is inserted in position start to position stop in the sample   
@@ -37,30 +32,30 @@ def crossOverZeros(start, stop, sample):
     return sample
 
 # Function that takes a current population and generates a better population
-def generateNextGeneration(population):
+def generateNextGeneration(population, replacedBits, Ei):
     
     # Convert population into a list of lists, in order to do bit operations
     for i in range(0, len(population)):
         population[i] = list(population[i])
         
     # i is the embedding position, corresponding with the first individual 
-    # in the population
+    # in the population. This is equal to paper value * -1 -1
     i = -4
-    
-    for j in range(0, 5):
         
+    print(replacedBits)
+    for j in range(0, 1):
         # If message bit (Ei) is zero
-        if (population[0][8] == '0'):
+        
+        if (Ei == '0'):
             
             # If data bit at i is zero, do nothing
-            if(population[j][i] == '0'):
+            if(replacedBits[j] == '0'):
                 continue
             
             else:
                 # If position bit of i is at -4, put 0 at i
                 if (i == -4):
                     population[j][i] = '0'
-                    
                     # If data at i - 1 is 0, then perform mutation operation
                     # at position i - 1
                     if (population[j][i - 1] == '0'):
@@ -70,61 +65,81 @@ def generateNextGeneration(population):
                 # Crossover from i-1 to 3 with 1...11111 string
                 if (i == -8):
                     population[j][i] = '0'
-                    population[j] = crossOverOnes(i - 1, -3, population[j])
+                    population[j][-7] = '1'
+                    population[j][-6] = '1'
+                    population[j][-5] = '1'
+                    population[j][-4] = '1'
                     
                 # Put 0 at position i and perform crossover from i-1 to 3 
                 # with 1...11111 string    
                 else:
                     population[j][i] = '0'
-                    population[j] = crossOverOnes(i - 1, -3, population[j])
-                    
+                    population[j][-7] = '1'
+                    population[j][-6] = '1'
+                    population[j][-5] = '1'
+                    population[j][-4] = '1'                    
                     # if data bit at i + 1 is 0, then perform mutation at 
                     # position i + 1 and crossover operation at position from 
                     # i-1 to 3 with 000.00000 string
                     if (population[j][i - 1] == '0'):
                         population[j][i - 1] = '1'
-                        population[j] = crossOverZeros(i - 1, -3, population[j])
-            
+                        population[j][-7] = '0'
+                        population[j][-6] = '0'
+                        population[j][-5] = '0'
+                        population[j][-4] = '0'
+                        
         # If the message bit (Ei) is one    
         else:
+            print("HI")
+            
             # If data bit at i is one, do nothing
-            if(population[j][i] == '1'):
+            if(replacedBits[j] == '1'):
                 continue
             
             else:
-                # If position bit of i is at -4, put 0 at i
+                # If position bit of i is at -4, put 1 at i
                 if (i == -4):
                     population[j][i] = '1'
-                    
+                    print("KAKA",population[j])
+
                     # If data at i - 1 is 1, then perform mutation operation
                     # at position i - 1
                     if (population[j][i - 1] == '1'):
                         population[j][i - 1] == '0'
+
                                         
                 # If position of i is at -8, put 1 in position i and perform
                 # Crossover from i-1 to 3 with 0...0000 string
                 if (i == -8):
                     population[j][i] = '1'
-                    population[j] = crossOverZeros(i - 1, -3, population[j])
-                    
+                    population[j][-7] = '0'
+                    population[j][-6] = '0'
+                    population[j][-5] = '0'
+                    population[j][-4] = '0'                    
                 # Put 1 at position i and perform crossover from i-1 to 3 
                 # with 0...000000 string    
                 else:
                     population[j][i] = '1'
-                    population[j] = crossOverZeros(i - 1, -3, population[j])
-                    
+                    print("KAKA",population[j])
+                    population[j][-7] = '0'
+                    population[j][-6] = '0'
+                    population[j][-5] = '0'
+                    population[j][-4] = '0'  
+                    print("KAKA",population[j])
                     # if data bit at i + 1 is 1, then perform mutation at 
                     # position i + 1 and crossover operation at position from 
                     # i-1 to 3 with 1111.1 string
                     if (population[j][i - 1] == '1'):
                         population[j][i - 1] = '0'
-                        population[j] = crossOverOnes(i - 1, -3, population[j])
-            
+                        population[j][-7] = '1'
+                        population[j][-6] = '1'
+                        population[j][-5] = '1'
+                        population[j][-4] = '1'            
         i = i - 1
             
     
-    for i in range(0, len(population)):
-        population[i] = "".join(population[i])
+    for l in range(0, len(population)):
+        population[l] = "".join(population[l])
     
     return population
 
