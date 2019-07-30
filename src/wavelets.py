46c91d99f596fd0441394f72b36bcbf406433fe9
Johan Gouws <u16033915@tuks.co.za>
2019-07-30 17:01:42 +0200

    Script to better understand wavelets and working python library

--- src/wavelets.py
+++ src/wavelets.py
@@ -1,29 +1,11 @@
-'''import wave
-
-import pylab
-def graph_spectrogram(wav_file):
-    sound_info, frame_rate = get_wav_info(wav_file)
-    pylab.figure(num=None, figsize=(19, 12))
-    pylab.subplot(111)
-    pylab.title('spectrogram of %r' % wav_file)
-    pylab.specgram(sound_info, Fs=frame_rate)
-    pylab.savefig('spectrogram.png')
-    
-def get_wav_info(wav_file):
-    wav = wave.open(wav_file, 'r')
-    frames = wav.readframes(-1)
-    sound_info = pylab.fromstring(frames, 'int16')
-    frame_rate = wav.getframerate()
-    wav.close()
-    return sound_info, frame_rate
-
-graph_spectrogram('C:/Users/project/Desktop/Audio-Steganograpy/src/Media/StegoSong.wav')
-'''
+# https://dsp.stackexchange.com/questions/47437/discrete-wavelet-transform-visualizing-relation-between-decomposed-detail-coef
 
 import pywt
 import pywt.data
 import numpy as np
 import matplotlib.pyplot as plt
+import fileprocessing as fp
+import wave
 
 plt.close('all')
 
@@ -40,13 +22,15 @@ def reconstruction_stem(yyy, xmax, **kwargs):
     ymax = yyy.max()
     plt.stem(np.linspace(0, 1., num=len(yyy)), yyy*(xmax/ymax), **kwargs)
 
+waveObject = wave.open('Media/opera.wav', 'rb')
+x = fp.extractWaveSamples(waveObject)
+x=np.asarray(x[0])
 
-x = pywt.data.ecg()
 w = pywt.Wavelet('sym5')
 nl = 6
 coeffs = pywt.wavedec(x, w, level=nl)
 
-
+print("Coeff gotten")
 '''
 plt.figure()
 plt.stem(coeffs[1]); plt.legend(['Lvl 6 detail coefficients'])
@@ -62,12 +46,23 @@ plt.figure()
 plt.stem(coeffs[6]); plt.legend(['Lvl 1 detail coefficients'])
 '''
 
-
+"""
 xmax = x.max()
 for i in range(nl):
     plt.figure()
-    reconstruction_plot(x) # original signal 
-    reconstruction_plot(pywt.waverec(coeffs[:i+2] + [None] * (nl-i-1), w)) # partial reconstruction 
-    #reconstruction_stem(coeffs[i+1], xmax, markerfmt ='none', linefmt='r-')
-    #plt.legend(['Original', ('Rec to lvl %d')%(nl-i), ('Details for lvl %d')%(nl-i)])
-    plt.legend(['Original', ('Rec to lvl %d')%(nl-i)])
+    #reconstruction_plot(x) # original signal 
+    reconstruction_plot(pywt.waverec(coeffs[:i+2] + [None] * (nl-i-1), w)) # partial reconstruction
+    reconstruction_stem(coeffs[i+1], xmax, markerfmt ='none', linefmt='r-')
+    plt.legend(['Original', ('Rec to lvl %d')%(nl-i), ('Details for lvl %d')%(nl-i)])
+    #plt.legend(['Original', ('Rec to lvl %d')%(nl-i)])
+    
+"""    
+print(len(coeffs[0]))
+print(len(coeffs[1]))
+print(len(coeffs[2]))
+print(len(coeffs[3]))
+print(len(coeffs[4]))
+print(len(coeffs[5]))
+
+plt.figure(55)
+reconstruction_plot(pywt.waverec(coeffs + [None] * 6, w)) # partial reconstruction   
