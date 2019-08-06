8509c7208056f77f9b9c9e6fc70780c4295f9f7a
Johan Gouws <u16033915@tuks.co.za>
2019-08-06 21:59:05 +0200

    Basic DWT working with reading from stego file. Bugs with long messages

--- src/DWT_LSB.py
+++ src/DWT_LSB.py
@@ -9,14 +9,19 @@ import pywt
 import fileprocessing as fp
 import wave
 import binascii
+import numpy as np
 
 def text_from_bits(bits, encoding='utf-8', errors='surrogatepass'):
     n = int(bits, 2)
-    return int2bytes(n).decode(encoding, errors)
+    if (n > 0 and n < 126):
+        return int2bytes(n).decode(encoding, errors)
 
+    else:
+        return 'x'
 def int2bytes(i):
     hex_string = '%x' % i
     n = len(hex_string)
+ 
     return binascii.unhexlify(hex_string.zfill(n + (n & 1)))
 
 # Function to place a single bit within a 16 bit cover sample
@@ -41,6 +46,8 @@ def replaceBit(sample, LSB_number, bit):
 def calcPower(coefficient):
     p = 0
     
+    coefficient = np.abs(coefficient)
+    
     for i in range(0,17):
         if (2 ** i > coefficient):
             p = i - 1
@@ -51,8 +58,15 @@ def calcPower(coefficient):
 # Function to embed a message within a single sample. Will start at 3rd LSB 
 def encodeCoefficient(sample, message):
         
-    for i in range(3, 3 + len(message)):
-        sample = replaceBit(sample, i, message[i - 3])
+    
+    sample = replaceBit(sample, 1, '1')  
+    sample = replaceBit(sample, 2, '0')  
+    sample = replaceBit(sample, 3, '1')  
+    
+        
+      
+    for i in range(4, 4 + len(message)):
+        sample = replaceBit(sample, i, message[i - 4])
         
     return sample
     
@@ -69,9 +83,9 @@ def extractBit(sample, LSB_number):
 def decodeCoefficient(sample, bits):
     msg = ''    
     
-    for i in range(3, 3 + bits):
+    for i in range(4, 4 + bits):
         msg += extractBit(sample, i)
-
+        
     return msg
 
 # Type of wavelet to use
@@ -87,9 +101,11 @@ samplesOne, samplesTwo = fp.extractWaveSamples(song)
 cA_1, cD_1 = pywt.wavedec(samplesOne, wavletType, level=1)
 
 # Write the message that is to be embedded
-message = "Good day"
+message = fp.getMessageBits('Media/text.txt')
+message = "".join(list(map(str, message)))
 
-# Add the length of the message to be encoded as the first 24 bits of the msg
+message = 'First DWT Text steganography encoded and extracted successufully\n'*400
+
 
 # Convert the message to a binary sequence
 message = fp.messageToBinary(message)
@@ -98,19 +114,26 @@ messageLength = len(message)
 messageLength = '{0:024b}'.format(messageLength)
 message = messageLength + message
 
-
+print(len(message), "Message bits encoded within", len(samplesOne), "samples")
+print(len(message)/8, "bits/second encoding")
+print("Capacity (%) =",len(message)/(len(samplesOne)*16)*100)
 
 extractMessage = ''
 
 # Amount of bits to keep of the coefficient
-OBH = 12
+OBH = 6
 
+print("############ EMBEDDING   #################")
 # Embed the message
 for i in range(0, len(cD_1)):
- 
     # Calculate the amount of bits that can possibly hidden
     replaceBits = calcPower(cD_1[i]) - OBH
-        
+   
+    if (len(message) > 1 and i == len(cD_1) - 1):
+          print("Message is too long")
+          break
+    
+    
     # If it returns as a negative amount, skip the sample
     if (replaceBits <= 0):
         continue
@@ -119,9 +142,8 @@ for i in range(0, len(cD_1)):
         # Get the amount of message bits that will be embedded
         embedMessage = message[:replaceBits]
         message = message[replaceBits:]
-        
-        # Replace the message in the coefficient of level 1 detailed coeff
         cD_1[i] = encodeCoefficient(int(cD_1[i]), embedMessage)
+        
 
         # If the message is embedded, break
         if (len(message) == 0):
@@ -131,46 +153,71 @@ for i in range(0, len(cD_1)):
 stegoSamples = pywt.waverec([cA_1, cD_1], wavletType)
 stegoSamples = list(map(int, stegoSamples))
 
+for i in range(len(stegoSamples)):
+      if (stegoSamples[i] > 32767):
+            stegoSamples[i] = 32767
+      
+      if (stegoSamples[i] < -32767):
+            stegoSamples[i] = -32767
+      
+print("############ WRITING   #################")
+
 # Write to the stego song file
 fp.writeStegoToFile('Media/DWT.wav',song.getparams(), stegoSamples)
 
+print("############ Reading   #################")
 
+
+print("############ EXTRACTING 1   #################")
 ####################   From stego file   ######################################
 
 # Open the cover audio
-song = wave.open('Media/DWT.wav', mode='rb')
+stego = wave.open('Media/DWT.wav', mode='rb')
 
 # Extract the wave samples from the host signal
-samplesOne, samplesTwo = fp.extractWaveSamples(song)
+samplesOneStego, samplesTwoStego = fp.extractWaveSamples(stego)
 
 # Get the approximate coefficients and detail coefficients of the signal
-cA_1_new, cD_1_new = pywt.wavedec(samplesOne, wavletType, level=1)
-
-
+cA_1_new, cD_1_new = pywt.wavedec(samplesOneStego, wavletType, level=1)
     
 extractedLength = 0
+foundMsgLength = False
 
-for i in range(0, len(cD_1_new)):
+extractMessage = ''
+
+for i in range(0, len(cD_1_new)):    
     replaceBits = calcPower(cD_1_new[i]) - OBH
-    
+       
     if (replaceBits <= 0):
         continue
     
     else:
-        print("{0:016b}".format(int(cD_1_new[i])), end = ' ')
-        #print(replaceBits, end = ' ')
         extractMessage += decodeCoefficient(cD_1_new[i], replaceBits)
-        #print(cD_1_new[i], end = ' ')
-        if (len(extractMessage) == 24):
-            extractedLength = int(extractMessage, 2)
-            break
+
+        if (len(extractMessage) >= 24 and foundMsgLength == False):
+            extractedLength = int(extractMessage[0:24], 2)
+            foundMsgLength = True
             
+        else:
+            if (len(extractMessage) >= extractedLength + 24 and foundMsgLength == True):
+                extractMessage = extractMessage[24:]
+                break
+        
+temp = ''
 
-print("")
-print("")
+while(len(extractMessage) >= 8):
+    temp += text_from_bits(extractMessage[:8])
+    extractMessage = extractMessage[8:]        
+        
+        
+print(temp)
+
+print("############ EXTRACTING 2   #################")
 #####################   From current  #########################################
-
+'''
 extractedLength = 0
+foundMsgLength = False
+
 extractMessage = ''
 
 for i in range(0, len(cD_1)):    
@@ -180,32 +227,72 @@ for i in range(0, len(cD_1)):
         continue
     
     else:
-        #print(replaceBits, end = ' ')
-
         extractMessage += decodeCoefficient(cD_1[i], replaceBits)
-        
-        if (len(extractMessage) == 24):
-            extractedLength = int(extractMessage, 2)
+
+        if (len(extractMessage) >= 24 and foundMsgLength == False):
+            extractedLength = int(extractMessage[0:24], 2)
+            foundMsgLength = True
             
         else:
-            print("{0:016b}".format(int(cD_1[i])), end = ' ')
-            if (len(extractMessage) == extractedLength + 24):
+            if (len(extractMessage) >= extractedLength + 24 and foundMsgLength == True):
                 extractMessage = extractMessage[24:]
                 break
         
 temp = ''
-for i in range(int(len(extractMessage)/8)):
+
+while(len(extractMessage) >= 8):
     temp += text_from_bits(extractMessage[:8])
     extractMessage = extractMessage[8:]        
+    
+#print(temp)
+
+
+'''
         
         
-print(temp)
 
 
 
 
-        
-        
-        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
         
