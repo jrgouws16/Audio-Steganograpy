7fb153d6af4cd46f66ccfea372a0a29e3e7ef319
Johan Gouws <u16033915@tuks.co.za>
2019-08-13 17:03:39 +0200

    Added client and server file sending data between each other

---
+++ src/server.py
@@ -0,0 +1,32 @@
+import socket
+import sys
+
+HOST = ""
+PORT = 9999
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(5)
+
+print("Listening ...")
+
+while True:
+    conn, addr = s.accept()
+    print("[+] Client connected: ", addr)
+
+    # get file name to download
+    f = open("file_received.pdf", "wb")
+    while True:
+        # get file bytes
+        data = conn.recv(4096)
+        if not data:
+            break
+        # write bytes on file
+        f.write(data)
+    f.close()
+    print("[+] Download complete!")
+
+    # close connection
+    conn.close()
+    print("[-] Client disconnected")
+    sys.exit()
