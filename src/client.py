7c8041e50652b134399b7878f1372501d3d28f77
Johan Gouws <u16033915@tuks.co.za>
2019-08-20 23:23:38 +0200

    Better client written able to communicate with non local host.

--- src/client.py
+++ src/client.py
@@ -1,23 +1,29 @@
 import socket
 
 
-HOST = "localhost"
-PORT = 9999
+HOST = "192.168.0.8"
+PORT = 9020
 
 s = socket.socket(socket.AF_INET,   socket.SOCK_STREAM)
+
 s.connect((HOST, PORT))
 print("[+] Connected with Server")
 
 # get file name to send
-f_send = "PDF_3.pdf"
+f_send = "Media/PDF_2.pdf"
 # open file
 with open(f_send, "rb") as f:
     # send file
     print("[+] Sending file...")
     data = f.read()
+    s.send("Hello from Windows".encode())
     s.sendall(data)
 
     # close connection
     s.close()
     print("[-] Disconnected")
     f.close()
+
+
+s.close()    
+
