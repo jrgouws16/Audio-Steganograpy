2795f583283ec1f48cf835849c8f4976372d92e4
Johan <u16033915@tuks.co.za>
2019-08-20 23:26:36 +0200

    Server written able to thread and communicate with non local client

--- src/server.py
+++ src/server.py
@@ -1,21 +1,13 @@
 import socket
 import sys
+import _thread
 
 HOST = ""
-PORT = 9999
+PORT = 9020
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(5)
-
-print("Listening ...")
-
-while True:
-    conn, addr = s.accept()
-    print("[+] Client connected: ", addr)
-
+def threaded_client(conn):
     # get file name to download
-    f = open("file_received.pdf", "wb")
+    f = open("/home/johan/Desktop/file_received.pdf", "wb")
     while True:
         # get file bytes
         data = conn.recv(4096)
@@ -29,4 +21,30 @@ while True:
     # close connection
     conn.close()
     print("[-] Client disconnected")
-    sys.exit()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(10)
+
+try:
+    s.bind((HOST, PORT))
+    
+except socket.error as e:
+    print(str(e))
+
+
+s.listen(5)
+
+print("Listening ...")
+
+while (True):
+    
+    try:
+        conn, addr = s.accept()
+        
+    except Exception:
+        print("Socket timed out")
+        conn.close()
+        break
+    
+    print('[+] Client connected:', addr)
+    start_new_thread(threaded_client, (conn,))
